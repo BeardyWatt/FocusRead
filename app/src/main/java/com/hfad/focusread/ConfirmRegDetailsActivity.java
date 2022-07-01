23adf4bbd1b0714d270d70a39215d24622cc206e
Breadyface <93210609+Breadyface@users.noreply.github.com>
2022-07-02 00:06:25 +0100

    layouts registration - congrats

---
+++ app/src/main/java/com/hfad/focusread/ConfirmRegDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.focusread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmRegDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confrim_reg_details);
+    }
+}
