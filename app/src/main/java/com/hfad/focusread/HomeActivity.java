50f69eb0317ec77acad338df88cdffbe4c2c10e3
Breadyface <93210609+Breadyface@users.noreply.github.com>
2022-07-08 20:20:25 +0100

    layout clean-up

--- app/src/main/java/com/hfad/focusread/HomeActivity.java
+++ app/src/main/java/com/hfad/focusread/HomeActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
@@ -18,6 +20,7 @@ public class HomeActivity extends AppCompatActivity {
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(HomeActivity.this,LoginActivity.class);
                 startActivity(intent);
             }
